5e6c9ecde03550eff1c14cf189004726b7efcda4
yashladekar <yashladekar002@gmail.com>
2023-07-02 02:15:46 +0530

    make changes to settings of django

--- bookstore/bookstore/settings.py
+++ bookstore/bookstore/settings.py
@@ -127,3 +127,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',  # Replace with your React frontend URL
+]
